a947fc4f81c6c17a719e603d2a0f847abc3b65d8
benhudds <benhudds@hotmail.co.uk>
2018-02-10 17:29:51 +0000

    Removed change migrations for the postgres migration

--- db/migrate/20180103234011_add_measure_to_iqps.rb
+++
@@ -1,5 +0,0 @@
-class AddMeasureToIqps < ActiveRecord::Migration[5.1]
-  def change
-    add_column :iqps, :measure, :string
-  end
-end
--- db/migrate/20180209121154_addimageurl.rb
+++
@@ -1,5 +0,0 @@
-class Addimageurl < ActiveRecord::Migration[5.1]
-  def change
-    add_column :ipls, :imageurl, :string
-  end
-end
