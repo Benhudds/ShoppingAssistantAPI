0ec1925c1f40d1e20fc4f4ad9d8e1025da2ce6a3
benhudds <benhudds@hotmail.co.uk>
2018-02-11 13:45:45 +0000

    Updated production url for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "shass-api_#{Rails.env}"
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host:'rails-tutorial-benhudds.c9users.io', port: '3000' }
+  config.action_mailer.default_url_options = { host:'https://ancient-mountain-71816.herokuapp.com', port: '3000' }
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
