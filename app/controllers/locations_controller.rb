ed775374103dbb0d87b57176a2c1568dfa917dd9
benhudds <benhudds@hotmail.co.uk>
2018-02-13 06:50:59 +0000

    Cleanup

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -9,7 +9,6 @@ class LocationsController < ApplicationController
     # If the user supplies a latitude and longitude value
     if (params[:lat] != nil && params[:lng] != nil)
       
-      Resque.enqueue(DeleteOldIplsController)
       print "\n"
       print "\n"
       print "\n"
