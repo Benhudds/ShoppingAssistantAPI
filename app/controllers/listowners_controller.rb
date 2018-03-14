3fcb02396410bdc687625c0ac176f3e16a94bec6
benhudds <benhudds@hotmail.co.uk>
2018-03-14 22:36:38 +0000

    Logging to check why multiple listowners are created

--- app/controllers/listowners_controller.rb
+++ app/controllers/listowners_controller.rb
@@ -22,7 +22,8 @@ class ListownersController < ApplicationController
           # Check if new user is already an owner
           if (Listowner.where(slist_id: :slist_id, user_id: newUser.id).blank?)
             print "\n"
-            print Listowner.where(:slist_id => params[:slist_id], user_id: newUser.id).first
+            print Listowner.where(:slist_id => params[:slist_id], user_id: newUser.id).blank?
+            print "\n"
             Listowner.create!(:slist_id => params[:slist_id], :user_id => newUser.id)
           end
           
